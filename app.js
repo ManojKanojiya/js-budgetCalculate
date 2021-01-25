1c1ef74925b8604297772a1a09a6b5863b70ba25
Manoj Kanojiya <manoj.softvan@gmail.com>
2021-01-25 15:11:08 +0530

    Added read data from html

--- app.js
+++ app.js
@@ -9,14 +9,35 @@ var budgetController = (function () {
 
 // UI CONTROLLER
 var UIController = (function () {
-    // Some Code 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+        getDOMstring: function () {
+            return DOMstrings;
+        }
+    }
 })();
 
 
 var controller = (function (budgetCtrl, UIctrl) {
+    var DOM = UIctrl.getDOMstring();
+
     var cntrlAddItem = function () {
         // 1. Get Input field data
-
+        var input = UIctrl.getInput();
+        console.log(input);
         // 2. Add item to budget controller
 
         // 3. Add items into UI
@@ -24,10 +45,10 @@ var controller = (function (budgetCtrl, UIctrl) {
         // 4. Calculate Budget
 
         // 5. Add that into UI
-        console.log('Cntrl Add Item!!!')
+
     };
 
-    document.querySelector('.add__btn').addEventListener('click', cntrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', cntrlAddItem);
 
     document.addEventListener('keypress', function (event) {
         if (event.keyCode === 13 || event.which === 13) {
